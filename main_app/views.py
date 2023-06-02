56dffd084ac7334268e6af80efb37db0cfc42586
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-06-01 23:10:58 -0500

    part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+pokemon = [
+  {'name': 'Pikachu', 'type': 'electric', 'description': 'pika pika type shit', 'age': 5},
+  {'name': 'Oshawott', 'type': 'water', 'description': 'cute fr', 'age': 2},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+   return render(request, 'about.html')
+
+def pokemon_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'pokemon/index.html', {
+    'pokemon': pokemon
+  })
--- pokemoncollector/settings.py
+++ pokemoncollector/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'pokemoncollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokemoncollector',
+        # 'HOST': 'localhost',  <-- some computers might need this line
     }
 }
 
